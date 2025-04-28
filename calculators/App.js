c8e2abc6a9b4108759191c0b566d5861cb4ee530
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-28 14:16:17 +0300

    Update App.js

--- calculators/App.js
+++ calculators/App.js
@@ -1,23 +1,14 @@
+// App.js
 import React from 'react';
 import { StatusBar, SafeAreaView, StyleSheet } from 'react-native';
-import { Provider } from 'react-redux';
-import store from './store';
-import { ApolloProvider } from '@apollo/client';
-import { client } from './apollo';
-
-// Import Clinical Calculators Component
-import ClinicalCalculators from './src/ClinicalCalculators';
+import ClinicalCalculators from './src/components/calculators/ClinicalCalculators';
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <ApolloProvider client={client}>
-        <SafeAreaView style={styles.safeArea}>
-          <StatusBar barStyle="light-content" backgroundColor="#004C54" />
-          <ClinicalCalculators />
-        </SafeAreaView>
-      </ApolloProvider>
-    </Provider>
+    <SafeAreaView style={styles.safeArea}>
+      <StatusBar barStyle="light-content" backgroundColor="#004C54" />
+      <ClinicalCalculators />
+    </SafeAreaView>
   );
 }
 

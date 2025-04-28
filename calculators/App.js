3f2e78bd00aebff7b383e30b1ec0b3c1285d6aee
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-28 14:24:08 +0300

    Update App.js

--- calculators/App.js
+++ calculators/App.js
@@ -1,13 +1,13 @@
 // App.js
 import React from 'react';
 import { StatusBar, SafeAreaView, StyleSheet } from 'react-native';
-import CHADSVASCCalculator from './src/components/calculators/neurology/CHADSVASc';
+import { CHADSVASc } from './src/components/calculators/neurology/CHADSVASc';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.safeArea}>
       <StatusBar barStyle="light-content" backgroundColor="#004C54" />
-      <CHADSVASCCalculator />
+      <CHADSVASc />
     </SafeAreaView>
   );
 }

64e710231906c15bb3197ac477d10e345f83474d
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-24 02:25:41 +0300

    Create Layout.jsx

---
+++ calculators/src/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import { Outlet } from 'react-router-dom';
+
+export default function Layout() {
+  return (
+    <div className="flex min-h-screen bg-white">
+      <Sidebar />
+      <main className="flex-1 p-6">
+        <Outlet />
+      </main>
+    </div>
+  );
+}

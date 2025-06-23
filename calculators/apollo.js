aea51de22a0348dbe97993c2c0a06a12bf6b7450
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-23 19:36:24 +0300

    Delete calculators/apollo.js

--- calculators/apollo.js
+++
@@ -1,7 +0,0 @@
-// apollo.js
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-export const client = new ApolloClient({
-  uri: 'https://your-graphql-endpoint.com/graphql', // Replace with actual endpoint or a placeholder
-  cache: new InMemoryCache(),
-});

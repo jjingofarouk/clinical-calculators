f5789c16848c03353e196594f28f763b3958b06d
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-23 18:36:15 +0300

    Update App.js

--- calculators/App.js
+++ calculators/App.js
@@ -1,20 +1,20 @@
-// App.js
 import React from 'react';
-import { StatusBar, SafeAreaView, StyleSheet } from 'react-native';
+import { CssBaseline, Box } from '@mui/material';
 import ClinicalCalculators from './src/components/calculators/ClinicalCalculators';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <StatusBar barStyle="light-content" backgroundColor="#004C54" />
-      <ClinicalCalculators />
-    </SafeAreaView>
+    <>
+      <CssBaseline />
+      <Box sx={{ 
+        flex: 1, 
+        backgroundColor: '#004C54', 
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column'
+      }}>
+        <ClinicalCalculators />
+      </Box>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    backgroundColor: '#004C54',
-  },
-});

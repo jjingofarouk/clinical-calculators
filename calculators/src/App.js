3b9d47c8b10b7b5895c0717645c4b994b23fb624
ZunoBotics <jjingofarouq@gmail.com>
2025-06-25 00:07:17 +0300

    Update App.js

--- calculators/src/App.js
+++ calculators/src/App.js
@@ -1,28 +1,63 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// Layout with responsive sidebar
 import Layout from './Layout';
-
-// Pages
 import Home from './Home';
+import NotFound from './components/NotFound';
+import { sidebarItems } from './data/sidebarItems';
 
-// Calculator Category Pages
-import GeneralCalculators from './components/calculators/general/GeneralCalculators';
-import CardiovascularCalculators from './components/calculators/cardiovascular/CardiovascularCalculators';
-import PulmonaryCalculators from './components/calculators/pulmonary/PulmonaryCalculators';
-import GastroenterologyCalculators from './components/calculators/git/GastroenterologyCalculators';
-import ObstetricsCalculators from './components/calculators/obstetrics/ObstetricsCalculators';
-import NeurologyCalculators from './components/calculators/neurology/NeurologyCalculators';
-import NephrologyCalculators from './components/calculators/nephrology/NephrologyCalculators';
-import OrthopedicsCalculators from './components/calculators/ortho/OrthopedicsCalculators';
-import ICUCalculators from './components/calculators/icu/ICUCalculators';
+// Dynamically import calculator category pages
+const calculatorCategories = [
+  'AllergyAndImmunology',
+  'Anesthesiology',
+  'BariatricSurgery',
+  'Cardiology',
+  'CardiothoracicSurgery',
+  'ColorectalSurgery',
+  'CriticalCare',
+  'Dermatology',
+  'EmergencyMedicine',
+  'Endocrinology',
+  'ENT',
+  'FamilyMedicine',
+  'Gastroenterology',
+  'GeneralSurgery',
+  'Geriatrics',
+  'Hematology',
+  'Hepatology',
+  'InfectiousDisease',
+  'InternalMedicine',
+  'MedicalGenetics',
+  'Nephrology',
+  'Neurology',
+  'Neurosurgery',
+  'Obstetrics',
+  'Oncology',
+  'Ophthalmology',
+  'Orthopedics',
+  'PainMedicine',
+  'PalliativeCare',
+  'Pathology',
+  'Pediatrics',
+  'PhysicalMedicine',
+  'PlasticSurgery',
+  'Psychiatry',
+  'Pulmonary',
+  'RadiationOncology',
+  'Rheumatology',
+  'SportsMedicine',
+  'Toxicology',
+  'TransplantMedicine',
+  'TraumaSurgery',
+  'Urology',
+  'VascularSurgery',
+];
 
-// 404 Fallback Page
-const NotFound = () => (
-  <div className="flex items-center justify-center h-full w-full bg-gray-50">
-    <h1 className="text-3xl font-bold text-gray-500">404 – Page Not Found</h1>
-  </div>
+// Create a mapping of category components
+const categoryComponents = Object.fromEntries(
+  calculatorCategories.map(category => [
+    category,
+    React.lazy(() => import(`./components/calculators/${category.toLowerCase()}/${category}Calculators`))
+  ])
 );
 
 export default function App() {
@@ -30,19 +65,26 @@ export default function App() {
     <div className="min-h-screen w-full bg-gray-50 p-0 m-0">
       <Router>
         <Routes>
-          {/* All pages wrapped in Layout */}
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
-            <Route path="/calculators/General" element={<GeneralCalculators />} />
-            <Route path="/calculators/Cardiovascular" element={<CardiovascularCalculators />} />
-            <Route path="/calculators/Pulmonary" element={<PulmonaryCalculators />} />
-            <Route path="/calculators/Gastroenterology" element={<GastroenterologyCalculators />} />
-            <Route path="/calculators/Obstetrics" element={<ObstetricsCalculators />} />
-            <Route path="/calculators/Neurology" element={<NeurologyCalculators />} />
-            <Route path="/calculators/Nephrology" element={<NephrologyCalculators />} />
-            <Route path="/calculators/Orthopedics" element={<OrthopedicsCalculators />} />
-            <Route path="/calculators/ICU" element={<ICUCalculators />} />
-            {/* Catch-all */}
+            {sidebarItems.map(item => {
+              const Component = categoryComponents[item.label.replace(/\s+/g, '')] || NotFound;
+              return (
+                <Route
+                  key={item.label}
+                  path={`/calculators/${item.label.replace(/\s+/g, '')}`}
+                  element={<Component />}
+                >
+                  {item.calculators.map(calc => (
+                    <Route
+                      key={calc}
+                      path={`/calculators/${item.label.replace(/\s+/g, '')}/${calc.replace(/\s+/g, '-')}`}
+                      element={<Component />}
+                    />
+                  ))}
+                </Route>
+              );
+            })}
             <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>

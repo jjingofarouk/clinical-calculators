a0593547680f5d65fd32c44ba8cb088ba6485317
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-24 03:01:21 +0300

    Update App.js

--- calculators/src/App.js
+++ calculators/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// Layout with Sidebar
+// Layout with responsive sidebar
 import Layout from './Layout';
 
 // Pages
 import Home from './Home';
 
-// Category Pages
+// Calculator Category Pages
 import GeneralCalculators from './components/calculators/general/GeneralCalculators';
 import CardiovascularCalculators from './components/calculators/cardiovascular/CardiovascularCalculators';
 import PulmonaryCalculators from './components/calculators/pulmonary/PulmonaryCalculators';
@@ -18,7 +18,7 @@ import NephrologyCalculators from './components/calculators/nephrology/Nephrolog
 import OrthopedicsCalculators from './components/calculators/ortho/OrthopedicsCalculators';
 import ICUCalculators from './components/calculators/icu/ICUCalculators';
 
-// 404 Not Found Page
+// 404 Fallback Page
 const NotFound = () => (
   <div className="flex items-center justify-center h-screen bg-white">
     <h1 className="text-3xl font-bold text-gray-500">404 – Page Not Found</h1>
@@ -27,27 +27,27 @@ const NotFound = () => (
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          {/* Home Page */}
-          <Route index element={<Home />} />
+    <div className="min-h-screen w-full overflow-hidden bg-white">
+      <Router>
+        <Routes>
+          {/* All pages wrapped in Layout */}
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/calculators/General" element={<GeneralCalculators />} />
+            <Route path="/calculators/Cardiovascular" element={<CardiovascularCalculators />} />
+            <Route path="/calculators/Pulmonary" element={<PulmonaryCalculators />} />
+            <Route path="/calculators/Gastroenterology" element={<GastroenterologyCalculators />} />
+            <Route path="/calculators/Obstetrics" element={<ObstetricsCalculators />} />
+            <Route path="/calculators/Neurology" element={<NeurologyCalculators />} />
+            <Route path="/calculators/Nephrology" element={<NephrologyCalculators />} />
+            <Route path="/calculators/Orthopedics" element={<OrthopedicsCalculators />} />
+            <Route path="/calculators/ICU" element={<ICUCalculators />} />
 
-          {/* Calculator Categories */}
-          <Route path="/calculators/General" element={<GeneralCalculators />} />
-          <Route path="/calculators/Cardiovascular" element={<CardiovascularCalculators />} />
-          <Route path="/calculators/Pulmonary" element={<PulmonaryCalculators />} />
-          <Route path="/calculators/Gastroenterology" element={<GastroenterologyCalculators />} />
-          <Route path="/calculators/Obstetrics" element={<ObstetricsCalculators />} />
-          <Route path="/calculators/Neurology" element={<NeurologyCalculators />} />
-          <Route path="/calculators/Nephrology" element={<NephrologyCalculators />} />
-          <Route path="/calculators/Orthopedics" element={<OrthopedicsCalculators />} />
-          <Route path="/calculators/ICU" element={<ICUCalculators />} />
-
-          {/* Fallback Route */}
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </Router>
+            {/* Catch-all */}
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
